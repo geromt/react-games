c7e9972a92952dc0f440b63d393b6e3f0d35b8b5
geromt <gerardomartinez205@gmail.com>
2025-02-15 14:38:45 -0600

    feat: Improve style of the project

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -2,7 +2,7 @@ export function Square ({ children, updateBoard, index }) {
   const handleClick = () => {updateBoard(index)}
 
   return (
-    <div onClick={handleClick} className='border-2 border-black w-5 h-5 flex justify-center items-center'>
+    <div onClick={handleClick} className='border-4 rounded-2xl border-highlight text-8xl w-40 h-40  flex justify-center items-center hover:bg-highlight/20'>
       {children}
     </div>
   )
